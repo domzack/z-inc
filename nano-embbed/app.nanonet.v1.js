a99fab3a2c2b9ccb346e40ee2b0977a42af4ee47
domzack <eusouzaca@gmail.com>
2024-07-26 14:17:36 +0000

    automatic push

---
+++ nano-embbed/app.nanonet.v1.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+})
+
+const comandos = [
+    {
+        cmd: 'cmd',
+        description: 'description',
+        help: 'help',
+        return: 'return'
+    },
+    {
+        cmd: 'VER',
+        description: 'Retorna versao do firmware',
+        help: 'VER',
+        return: 'Version'
+    },
+    {
+        cmd: 'STATUS',
+        description: 'Retorna status do dispositivo',
+        help: 'STATUS',
+        return: 'status'
+    },
+    {
+        cmd: 'SM',
+        description: 'Seta Modo Operacao. 0.: PULSADOR; 1.: PORTARIA',
+        help: 'Use SM.0, SM.1',
+        return: 'Value of SM'
+    },
+    {
+        cmd: 'SF',
+        description: 'Seta Tempo Verificacao Sensor FECHAMENTO',
+        help: 'Use SF.0 (0 segundos), SF.25 (2.5 segundos), SF.50 (5.0 segundos)',
+        return: 'Value of SM'
+    },
+]
